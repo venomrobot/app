011974716c1f01f5acb260839b69bdb3e1995287
Sander Groot Wesseldijk <Sander.gr.Wesseldijk@topicus.nl>
2018-09-07 17:45:08 +0200

    Avaible button stuff

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
+bool available = false;
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -11,66 +13,104 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: new MyHomePage(title: 'App Home'),
+      home: new HomePage(title: 'App Home'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _HomePageState createState() => new _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
+  //todo delete
   void refresh() {
     setState(() {});
   }
 
-  void openCamera() {
-
+  void _changeAvailable() {
+    available = !available;
+    print(available);
   }
+
+//  void openCamera() {}
 
   @override
   Widget build(BuildContext context) {
     TextStyle buttonTextStyle = new TextStyle(color: Colors.white);
 
-
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text(widget.title, style: buttonTextStyle,),
+        title: new Text(
+          widget.title,
+          style: buttonTextStyle,
+        ),
       ),
       body: new Center(
         child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new RaisedButton(
-              child: new Text("Button1", style: buttonTextStyle),
-              color: Theme
-                  .of(context)
-                  .accentColor,
-              splashColor: Colors.yellowAccent,
-              onPressed: refresh,
+            ButtonTheme(
+              minWidth: MediaQuery.of(context).size.width * 0.90,
+              child: new RaisedButton(
+                child: new Text("Beschikbaarheid instellen",
+                    style: buttonTextStyle),
+                color: Theme.of(context).accentColor,
+                splashColor: Colors.yellowAccent,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => AvailabilityScreen()),
+                  );
+                },
+              ),
             ),
-            new RaisedButton(
-              child: new Text("Button2", style: buttonTextStyle),
-              color: Theme
-                  .of(context)
-                  .accentColor,
-              splashColor: Colors.yellowAccent,
-              onPressed: refresh,
+            ButtonTheme(
+              minWidth: MediaQuery.of(context).size.width * 0.90,
+              child: new RaisedButton(
+                child: new Text("Nieuwe controle", style: buttonTextStyle),
+                color: Theme.of(context).accentColor,
+                splashColor: Colors.yellowAccent,
+                onPressed: refresh,
+              ),
             ),
-            new RaisedButton(
-              child: new Text("Button3", style: buttonTextStyle),
-              color: Theme
-                  .of(context)
-                  .accentColor,
-              splashColor: Colors.yellowAccent,
-              onPressed: refresh,
+            ButtonTheme(
+              minWidth: MediaQuery.of(context).size.width * 0.90,
+              child: new RaisedButton(
+                child: new Text("Controleren", style: buttonTextStyle),
+                color: Theme.of(context).accentColor,
+                splashColor: Colors.yellowAccent,
+                onPressed: refresh,
+              ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class AvailabilityScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Availability"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            _HomePageState()._changeAvailable();
+            Navigator.pop(context);
+          },
+          child: available
+              ? Text('I am available!')
+              : Text('I am no longer available'),
         ),
       ),
     );
